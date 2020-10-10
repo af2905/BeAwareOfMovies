074abafda3f6cbbadd62c589798c5361e605c57d
af2905 <af2905g@gmail.com>
2020-10-10 22:51:44 +0300

    Refactoring of the code in feedFragment.

--- app/src/main/java/com/af2905/beawareofmovies/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/af2905/beawareofmovies/ui/feed/FeedFragment.kt
@@ -10,15 +10,16 @@ import com.af2905.beawareofmovies.Constants.SEARCH_QUERY
 import com.af2905.beawareofmovies.R
 import com.af2905.beawareofmovies.data.Movie
 import com.af2905.beawareofmovies.network.MovieApiClient
+import com.af2905.beawareofmovies.observeOnMainThread
 import com.af2905.beawareofmovies.ui.observeOnMainThread
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
-import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.functions.Function4
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.feed_fragment.*
 import kotlinx.android.synthetic.main.feed_header.*
-import timber.log.Timber
 import java.util.concurrent.TimeUnit
 
 class FeedFragment : Fragment() {
@@ -42,10 +43,7 @@ class FeedFragment : Fragment() {
         language = resources.getString(R.string.language)
         movies_recycler_view.layoutManager = LinearLayoutManager(context)
         getSearchQuery()
-        getPopularMovies()
-        getNowPlayingMovies()
-        getUpcomingMovies()
-        getTopRatedMovies()
+        downloadMovies()
     }
 
     private fun getSearchQuery() {
@@ -86,92 +84,41 @@ class FeedFragment : Fragment() {
         findNavController().navigate(R.id.movie_details_fragment, bundle, options)
     }
 
-    private fun getPopularMovies() {
-        compositeDisposable.add(MovieApiClient.apiClient.getPopularMovies(language = language)
-            .subscribeOn(Schedulers.io())
-            .map { it.results }
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                Timber.tag("TEST_OF_LOADING_DATA").d(it.toString())
-                val popularMovies = listOf(
+    private fun downloadMovies() {
+        val apiClient = MovieApiClient.apiClient
+        compositeDisposable.add(Single.zip(
+            apiClient.getPopularMovies(language = language),
+            apiClient.getNowPlayingMovies(language = language),
+            apiClient.getUpcomingMovies(language = language),
+            apiClient.getTopRatedMovies(language = language),
+            Function4 { t1, t2, t3, t4 ->
+                return@Function4 listOf(
                     MainCardContainer(
-                        R.string.popular,
-                        it
-                            .filter { movie -> movie.title != null }
+                        R.string.popular, t1.results.filter { movie -> movie.title != null }
+                            .map { movie -> MovieItem(movie) { openMovieDetails(movie) } }.toList()
+                    ),
+                    MainCardContainer(
+                        R.string.now_playing, t2.results.filter { movie -> movie.title != null }
+                            .map { movie -> MovieItem(movie) { openMovieDetails(movie) } }.toList()
+                    ),
+                    MainCardContainer(
+                        R.string.upcoming, t3.results.filter { movie -> movie.title != null }
+                            .map { movie -> MovieItem(movie) { openMovieDetails(movie) } }.toList()
+                    ),
+                    MainCardContainer(
+                        R.string.top_rated, t4.results.filter { movie -> movie.title != null }
                             .map { movie -> MovieItem(movie) { openMovieDetails(movie) } }.toList()
                     )
                 )
-                movies_recycler_view.adapter = adapter.apply { addAll(popularMovies) }
-            }, {
-                Timber.tag("TEST_OF_LOADING_DATA").d(it.toString())
-            })
+            }
         )
-    }
+            .subscribeOn(Schedulers.io())
+            .observeOnMainThread()
+            .subscribe({
+                movies_recycler_view.adapter = adapter.apply { addAll(it) }
+            }, {
 
-    private fun getNowPlayingMovies() {
-        compositeDisposable.add(MovieApiClient.apiClient.getNowPlayingMovies(language = language)
-            .subscribeOn(Schedulers.io())
-            .map { it.results }
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                Timber.tag("TEST_OF_LOADING_DATA").d(it.toString())
-                val nowPlayingMovies = listOf(
-                    MainCardContainer(
-                        R.string.now_playing,
-                        it
-                            .filter { movie -> movie.title != null }
-                            .map { movie -> MovieItem(movie) { openMovieDetails(movie) } }.toList()
-                    )
-                )
-                movies_recycler_view.adapter = adapter.apply { addAll(nowPlayingMovies) }
-            }, {
-                Timber.tag("TEST_OF_LOADING_DATA").d(it.toString())
-            })
-        )
-    }
-
-    private fun getUpcomingMovies() {
-        compositeDisposable.add(MovieApiClient.apiClient.getUpcomingMovies(language = language)
-            .subscribeOn(Schedulers.io())
-            .map { it.results }
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                Timber.tag("TEST_OF_LOADING_DATA").d(it.toString())
-                val upcomingMovies = listOf(
-                    MainCardContainer(
-                        R.string.upcoming,
-                        it
-                            .filter { movie -> movie.title != null }
-                            .map { movie -> MovieItem(movie) { openMovieDetails(movie) } }.toList()
-                    )
-                )
-                movies_recycler_view.adapter = adapter.apply { addAll(upcomingMovies) }
-            }, {
-                Timber.tag("TEST_OF_LOADING_DATA").d(it.toString())
-            })
-        )
-    }
-
-    private fun getTopRatedMovies() {
-        compositeDisposable.add(MovieApiClient.apiClient.getTopRatedMovies(language = language)
-            .subscribeOn(Schedulers.io())
-            .map { it.results }
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                Timber.tag("TEST_OF_LOADING_DATA").d(it.toString())
-                val topRatedMovies = listOf(
-                    MainCardContainer(
-                        R.string.top_rated,
-                        it
-                            .filter { movie -> movie.title != null }
-                            .map { movie -> MovieItem(movie) { openMovieDetails(movie) } }.toList()
-                    )
-                )
-                movies_recycler_view.adapter = adapter.apply { addAll(topRatedMovies) }
-            }, {
-                Timber.tag("TEST_OF_LOADING_DATA").d(it.toString())
-            })
-        )
+            }))
     }
 
     override fun onStop() {
